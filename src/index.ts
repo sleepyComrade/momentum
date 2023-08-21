6e0df58c128d08fa2f96ad93750799f6dbb199d1
sleepyComrade <ivanovichivanivanov@protonmail.ch>
2023-08-21 14:22:48 +0200

    add dist to gh-pages

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import './style.css';
-import './owfont-regular.css';
-import { App } from "./components/app";
-
-const root = document.getElementById('root');
-const app = new App(root, 'app-wrap');
